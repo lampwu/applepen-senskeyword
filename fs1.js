845657d55b5bbb46e094cfdfae60b0d6d5dc7ef6
lampwu <lamp7850053@gmail.com>
2018-11-05 12:52:47 +0800

    Delete fs1.js

--- fs1.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs')
-fs.readFile('./ss.txt', (err, data) => {
-    if (err) throw err;
-    console.log(data);
-    let  k = data.toString('utf8',0,data.length)
-    let res = k.split(/[\n\r]+/);
-    console.log(res[0])  
-  });
