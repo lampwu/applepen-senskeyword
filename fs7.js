5b773a1bb6c33ec0d3496bdfa391eb838ae869e6
lampwu <lamp7850053@gmail.com>
2018-11-05 10:26:14 +0800

    增加fs7.js
    通过fs7.js对比im.fs生成的两个txt文件，console.log 敏感词

---
+++ fs7.js
@@ -0,0 +1,17 @@
+const fs = require('fs')
+
+let res = []
+let ccs = []
+let end = []
+const q = fs.readFileSync('./message.txt')
+let  k = q.toString('utf8',0,q.length)
+res = k.split(/[\n\r]+/);
+const p = fs.readFileSync('./ss.txt')
+let  j = p.toString('utf8',0,p.length)
+ccs = j.split(/[\n\r]+/);
+for (let i = 0;i<res.length;i++) {
+    if(res[i]==='{"head":{"status":"200","data":{}},"body":{"content":{"hasErrors":true,"errors":{"0":"不得含有不适当的词句。"},"partNumber":"MU8F2CH/A"}}}') {
+        end.push(ccs[i])
+    }
+  }
+console.log(end)
